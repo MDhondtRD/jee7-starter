5a4b769a86c0223f590aef865306083fe9c46e44
unknown <MDNAX30@WP2009.ATOMIUM.local>
2015-09-09 11:57:32 +0200

    poging oef2

---
+++ ejb-module/src/test/java/com/realdolmen/course/Passenger.java
@@ -0,0 +1,64 @@
+package com.realdolmen.course;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.io.Serializable;
+
+/**
+ * Created by MDNAX30 on 9/09/2015.
+ */
+@Entity
+public class Passenger implements Serializable{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String ssn;
+    private String firstName;
+    private String lastName;
+    private Integer frequentFlyerMiles;
+
+    Passenger(){
+
+    }
+
+    public Passenger()
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getFrequentFlyerMiles() {
+        return frequentFlyerMiles;
+    }
+
+    public void setFrequentFlyerMiles(Integer frequentFlyerMiles) {
+        this.frequentFlyerMiles = frequentFlyerMiles;
+    }
+}
---
+++ ejb-module/src/test/java/com/realdolmen/course/persistence/PassengerRepositoryTest.java
@@ -0,0 +1,7 @@
+package com.realdolmen.course.persistence;
+
+/**
+ * Created by MDNAX30 on 9/09/2015.
+ */
+public class PassengerRepositoryTest {
+}
